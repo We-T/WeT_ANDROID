9c8ffa0ad460e7ba7c71f764a3e67e5a1fbb5369
JiHyeonMon <jhk8806@naver.com>
2021-08-10 00:14:38 +0900

    chore : Change Recycler Padding Value in PlaceHomeActivity.kt
    - PlaceHomeActivity의 Recyclerview에 패딩 값으로 스크롤 막혀보였던 부분 수정 + overScrollMode 수정

--- app/src/main/java/com/wetour/we_t/ui/user/SignInActivity.kt
+++ app/src/main/java/com/wetour/we_t/ui/user/SignInActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.wetour.we_t.R
+import com.wetour.we_t.ui.placeHome.PlaceHomeActivity
 import com.wetour.we_t.ui.placeInfo.PlaceInfoActivity
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
@@ -56,7 +57,7 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.act_signin_btn_google -> {
-                val intent = Intent(this, SelectSignInMode::class.java)
+                val intent = Intent(this, PlaceHomeActivity::class.java)
                 startActivity(intent)
             }
         }
