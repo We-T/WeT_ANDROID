3683a4f30becf9b6d1c923dbcdd35006088fcf19
JiHyeonMon <jhk8806@naver.com>
2021-08-23 04:00:10 +0900

    feat : Custom Tab and Dynamic Fragment in MakeScheduleActivity.kt
    - 일정짜기 버튼 날짜 커스텀 완료
    - CustomViewPager.kt로 슬라이드 없애기
    - 동적으로 day 수에 맞게 Button 및 Fragment 설정

---
+++ app/src/main/java/com/wetour/we_t/ui/makeSchedule/CustomViewPager.kt
@@ -0,0 +1,41 @@
+package com.wetour.we_t.ui.makeSchedule
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+
+class CustomViewPager : ViewPager {
+    private var enables = false
+    private var isPagingEnabled = true
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    constructor(context: Context) : super(context)
+
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        if (enables) return super.onInterceptTouchEvent(ev)
+        else {
+            if (ev!!.action == MotionEvent.ACTION_MOVE) {
+
+            } else if (super.onInterceptTouchEvent(ev)) super.onTouchEvent(ev)
+        }
+        return false
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        if (enables) return super.onTouchEvent(ev)
+        else return ev!!.action != MotionEvent.ACTION_MOVE && super.onTouchEvent(ev)
+    }
+
+    fun setEnable(enable: Boolean) {
+        this.enables = enable
+    }
+
+    fun setPagingEnabled(b: Boolean) {
+        this.isPagingEnabled = b
+    }
+
+}
---
+++ app/src/main/java/com/wetour/we_t/ui/makeSchedule/ScheduleAdapter.kt
@@ -0,0 +1,16 @@
+package com.wetour.we_t.ui.makeSchedule
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class ScheduleAdapter(fm: FragmentManager, private val fragmentCount: Int) :
+    FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getItem(position: Int): Fragment {
+        return ScheduleFragment(position)
+    }
+
+    override fun getCount(): Int = fragmentCount
+
+}
