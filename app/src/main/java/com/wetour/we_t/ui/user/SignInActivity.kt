0672aa2f5d64efc3c4ab18ce50b8739e7a21112b
JiHyeonMon <jhk8806@naver.com>
2021-10-23 19:56:38 +0900

    feat: Server - Signin 완료

--- app/src/main/java/com/wetour/we_t/ui/user/SignInActivity.kt
+++ app/src/main/java/com/wetour/we_t/ui/user/SignInActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import com.wetour.we_t.R
 import com.wetour.we_t.network.RequestToServer
+import com.wetour.we_t.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import org.json.JSONObject
 import retrofit2.Call
@@ -23,7 +25,7 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when(v?.id) {
-            R.id.act_singin_btn_signup -> {
+            R.id.act_signin_btn_login -> {
 
                 val jsonData = JSONObject()
                 jsonData.put("email", act_signin_edit_id.text.toString())
@@ -36,21 +38,33 @@ class SignInActivity : AppCompatActivity(), View.OnClickListener {
                         call: Call<JsonObject>,
                         response: Response<JsonObject>
                     ) {
-                        Log.e("success", "${response.body()}")
                         if (response.isSuccessful) {
+                            Log.e("SignInActivity", "onResponse ${response.body().toString()}")
                             val res = response.body()
                             if (res!!.get("code").asString == "200") {
-                                val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+                                val intent = Intent(this@SignInActivity, HomeActivity::class.java)
                                 startActivity(intent)
+                            } else if (res!!.get("code").asString == "204") {
+                                // 비밀번호 틀림 || 아이디 존재하지 않음
+                                Toast.makeText(this@SignInActivity, "아이디와 비밀번호를 확인해주세요!", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(this@SignInActivity, "서버에서 알 수 없는 코드 수신", Toast.LENGTH_SHORT).show()
                             }
+                        } else {
+                            Log.e("SignInActivity", "onResponse-fail ${response.message()}")
+                            Toast.makeText(this@SignInActivity, "서버와 통신이 실패했습니다.", Toast.LENGTH_SHORT).show()
                         }
                     }
 
                     override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                        Log.e("fail", "${t.message}")
+                        Log.e("SignInActivity", "onFailure ${t.message}")
                     }
 
                 })
+            }
+            R.id.act_singin_btn_signup -> {
+                val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+                startActivity(intent)
             }
         }
     }
